82a2867616b2b13c1d1426cf2ba63722a74a46d7
Nghia Nguyen <nghianguyen161504@gmail.com>
2024-12-02 00:33:52 +0700

    Nghia | add install axios and fix css

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
-// src/axios.js
+
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/tasks', // Đảm bảo đúng URL của backend
+  baseURL: 'http://localhost:5000/api/tasks', 
 });
 
 export default axiosInstance;
